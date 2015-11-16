b5d99a1f21beca5b16bd2e9bb2c700a7a322531e
mstupak <>
2015-11-15 22:23:57 -0800

    Initial version

---
+++ TwoByTwoCube.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cubesolver.cubesolver;
+
+import cubesolver.*;
+
+/**
+ *
+ * @author Mike
+ */
+public class TwoByTwoCube {
+    
+    Face faces[];
+    
+    public TwoByTwoCube()
+    {
+        faces = new Face[6];
+    }
+    
+    public set(Face f, Color, zero, Color one, Color two, Color three)
+    {
+        
+    }
+    
+    public String toString()
+    {
+        return "testing";
+    }
+    
+}
