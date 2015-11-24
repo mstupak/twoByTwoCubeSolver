c048cd74bf4c37d6f147e14b2e562d9cef8ea661
mstupak <stupak@STUPAK2.auth.hpicorp.net>
2015-11-24 13:29:15 -0800

    another update

---
+++ Color.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cubesolver.cubesolver;
+
+import cubesolver.*;
+
+/**
+ *
+ * @author Mike
+ */
+public enum Color
+{
+    W, R, B, O, G, Y;
+    
+    public String toString() 
+    {
+        return this.name();
+    }
+}
--- cubesolver/cubesolver/MoveMap.java
+++ cubesolver/cubesolver/MoveMap.java
@@ -5,6 +5,7 @@
  */
 package cubesolver.cubesolver;
 
+import cubesolver.cubesolver.FaceName;
 import cubesolver.cubesolver.Move;
 import cubesolver.cubesolver.Turn;
 /**
@@ -12,9 +13,9 @@ import cubesolver.cubesolver.Turn;
  * @author stupak
  */
 public class MoveMap {
-    Move[][] moveArray = new Move[6][3];
+    Move moveArray[6][3];
     
-    public MoveMap()
+    public MoveMap
     {
         Move m;
         
@@ -46,7 +47,4 @@ public class MoveMap {
     }
     
     public Move getMoveList(FaceName f, Turn t)
-    {
-        return moveArray[f][t];
-    }
 }
